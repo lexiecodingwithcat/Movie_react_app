2f007a7a257b4036783a2d275d763e826b929f76
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-18 15:47:25 -0700

    useState and useEffect Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //never change state manually, it can only be changed by its own setter function
+  const [counter, setCounter] = useState(0);
+  //execute side effect when the component renders
+  //side effect: functions which is irrelevant with component render and state
+  //useEffect(callback function, dependency array)
+  //dependency array decides whento triger the side effect
+  useEffect(() => {
+    alert("you changed the counter to" + counter);
+  }, [counter]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter((prevCount) => prevCount + 1);
+        }}
+      >
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default App;

2d53ef89227fe4fa14cc82fa941d9500b0439080
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-19 15:44:27 -0700

    finished title and search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { useEffect } from "react";
 
+import "./App.css";
+import SearchIcon from "./search.svg";
+
 //a3efb870
-const API_URL = 'http://www.omdbapi.com?apikey=a3efb870';
+const API_URL = "http://www.omdbapi.com?apikey=a3efb870";
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+};
 
 const App = () => {
   // 2. function fetch movies
@@ -19,7 +30,37 @@ const App = () => {
     searchMovies("Spiderman");
   }, []);
 
-  return (<h1>App</h1>);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search for movies" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            {/* if there is no image, it will render a placeholder 400*400 image */}
+            <img
+              src={
+                movie1.Poster !== "N / A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 //export so that it can be called anywhere else

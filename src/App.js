2143264f7b156c08d62249560006d777fd091c76
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-19 12:31:51 -0700

    asyn() and await() to search movies by title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
+
+//a3efb870
+const API_URL = 'http://www.omdbapi.com?apikey=a3efb870';
 
 const App = () => {
-  return <h1>App</h1>;
+  // 2. function fetch movies
+  //async() function is asynchronously function, use title as parameter
+  //The await keyword is used to wait for the response of the HTTP request to return,
+  //and then stores the response object in the response variable.
+  const searchMovies = async (title) => {
+    // await
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+  //1.fetch the API as soon as the component loads
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (<h1>App</h1>);
 };
 
 //export so that it can be called anywhere else
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+// import {createRoot} from 'react-dom/client'
 
-import App from "./App.js"
+import App from "./App.js";
+// const root = createRoot(document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
+// root.render(<App />)

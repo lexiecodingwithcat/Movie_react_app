9736d613c97303fd40de07053b7f90b05ba1a0c7
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-19 17:06:09 -0700

    final version ><

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import SearchIcon from "./search.svg";
 
 //a3efb870
 const API_URL = "http://www.omdbapi.com?apikey=a3efb870";
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
+
 
 const App = () => {
   //use state to pass data from console to the movie card
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -3,21 +3,20 @@ import React from "react";
 const MovieCard = ({movie}) => {
    
   return (
-    <div className="movie">
+    <div className="movie" key={movie.imdbID}>
       <div>
         <p>{movie.Year}</p>
       </div>
+
       <div>
         {/* if there is no image, it will render a placeholder 400*400 image */}
         <img
           src={
-            movie.Poster !== "N / A"
+            movie.Poster !== "N/A"
               ? movie.Poster
-              : "https://via.placeholder.com/400"
-          }
-          alt={movie.Title}
-        />
+              : "https://via.placeholder.com/400"} alt={movie.Title}/>
       </div>
+      
       <div>
         <span>{movie.Type}</span>
         <h3>{movie.Title}</h3>
